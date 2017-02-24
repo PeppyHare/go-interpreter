37fcf32b33b6e2d8aae83aa78cc004bcf6fe6da8
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-02-24 13:07:42 -0800

    Passing all the tests! Automated commit :)

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -3,6 +3,8 @@ Abstract Syntax Tree for the Monkey language interpreter
 */
 package ast
 
+import "monkey/token"
+
 type Node interface {
 	TokenLiteral(string)
 }
@@ -15,4 +17,19 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
 }
