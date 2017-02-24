6f31223827233c3c31d6842210382c967b18732f
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-02-24 12:50:27 -0800

    Passing all the tests! Automated commit :)

--- runtests.sh
+++ runtests.sh
@@ -2,18 +2,23 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GOPATH="$DIR"
 
+formatCode() {
+	cd "$DIR" || exit
+	gofmt .
+}
+
 commitCode() {
 	cd "$DIR" || exit
 	git add .
 	git commit -m "Passing all the tests! Automated commit :)"
 }
 
-go test "./src/monkey/lexer" && \
- go test "./src/monkey/lexer"
+go test "./src/monkey/lexer"
 STATUS=$?
 if [[ $STATUS == "0" ]]; then
 	echo "Passing tests!"
 	echo "I am in this directory: $(pwd)"
+	formatCode
 	commitCode
 else
 	echo "Not passing tests... :("
