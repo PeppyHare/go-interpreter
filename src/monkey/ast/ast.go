adacc9ba9dc1f4fb663282b791bea2306092f872
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-02-24 13:16:22 -0800

    Passing all the tests! Automated commit :)

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -44,3 +44,6 @@ type Identifier struct {
 	Token token.Token // the token.IDENT token
 	Value string
 }
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
