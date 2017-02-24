1f5ff74a7e6a9638fc320fe37c597bea3c5800c9
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-02-24 13:14:50 -0800

    Passing all the tests! Automated commit :)

--- runtests.sh
+++ runtests.sh
@@ -15,7 +15,8 @@ commitCode() {
 	return 0 # just in case nothing to commit
 }
 
-echo "$############### $(date) Testing out new changes now :) ###############"
+echo ""
+echo "$(date) :  Testing out new changes now :) ###############"
 
 go test "./src/monkey/lexer"
 STATUS=$?
