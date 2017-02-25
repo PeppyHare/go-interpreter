ced1eb2c876380b6b31c6f6b88fa2557e2e9e311
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-02-24 18:39:34 -0800

    Passing all the tests! Automated commit :)

--- runtests.sh
+++ runtests.sh
@@ -21,7 +21,7 @@ echo "$(date) :  Testing out new changes now :)"
 go test -v "./..."| sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
 STATUS=$?
 if [[ $STATUS == "0" ]]; then
-	printf "\033[32mPassing tests!\033[0m"
+	printf "\033[32mPassing tests!\033[0m\n"
 	echo "I am in this directory: $(pwd)"
 	formatCode
 	commitCode
