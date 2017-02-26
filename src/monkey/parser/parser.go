b1565bd620a8d22e52c5037caa511624ea6aef8f
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-02-25 19:31:24 -0800

    Passing all the tests! Automated commit :)

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -18,6 +18,9 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -32,6 +35,11 @@ func New(l *lexer.Lexer) *Parser {
 
 	return p
 }
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
 
 func (p *Parser) Errors() []string {
 	return p.errors
@@ -124,4 +132,12 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
