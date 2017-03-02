e99bb109aeb43501a4c664d1a2c5361d15ee7543
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-03-01 21:53:35 -0800

    Return statements are returning stuff

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -24,6 +24,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.BlockStatement:
 		return evalStatements(node.Statements)
 
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
+
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
@@ -48,6 +52,10 @@ func evalStatements(stmts []ast.Statement) object.Object {
 
 	for _, statement := range stmts {
 		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
 	}
 
 	return result
