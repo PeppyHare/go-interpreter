0fcb08e5fedd8dba974095131e46b7cb124c722e
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-03-01 23:07:29 -0800

    Quick and dirty function evaluation

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -39,6 +39,11 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 		env.Set(node.Name.Value, val)
 
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
+
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
