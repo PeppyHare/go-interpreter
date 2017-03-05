b965c417f4012414b7e5d984d43fdb2675d285a2
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-03-05 02:16:43 -0800

    Strings being evaluated, too!

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -56,6 +56,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		body := node.Body
 		return &object.Function{Parameters: params, Env: env, Body: body}
 
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
+
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
@@ -86,6 +89,7 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+
 	}
 
 	return nil
